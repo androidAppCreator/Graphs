d73369559d877100d54849dc1821ca52156d247e
SwapnilCAmaha <110976641+SwapnilCAmaha@users.noreply.github.com>
2024-03-19 11:07:49 +0530

    Initial Commit : Pie Chart fully functional code

--- app/src/main/java/com/droid/ui/graphs/MainActivity.kt
+++ app/src/main/java/com/droid/ui/graphs/MainActivity.kt
@@ -9,14 +9,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -50,7 +48,7 @@ fun PieChartView(pieChartEntries: ArrayList<PieChartEntry>) {
         Spacer(modifier = Modifier.height(25.dp))
         PieChart(pieChartEntries = pieChartEntries, animDuration = 2000, pieChartSize = 180.dp)
         Spacer(modifier = Modifier.height(25.dp))
-        PieChartLegend(entries = pieChartEntries)
+        PieChartLegend(pieChartEntries = pieChartEntries)
         Spacer(modifier = Modifier.height(25.dp))
     }
 }
